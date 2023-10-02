fb4c52c183bd42e0e9d9de1a802ff56eaf67fa2f
User1 <ivankanev2@abv.bg>
2023-10-02 10:24:36 +0300

    added button textt

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -17,7 +17,34 @@ public class HelloController {
         welcomeText.setText(welcomeText.getText() + x);
     }
 
-    public void onDigitClick(ActionEvent actionEvent) {
+    public void onDigitClick1(ActionEvent actionEvent) {
         digits.setText(digits.getText() + 1);
+    }
+    public void onDigitClick2(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 2);
+    }
+    public void onDigitClick3(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 3);
+    }
+    public void onDigitClick4(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 4);
+    }
+    public void onDigitClick5(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 5);
+    }
+    public void onDigitClick6(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 6);
+    }
+    public void onDigitClick7(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 7);
+    }
+    public void onDigitClick8(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 8);
+    }
+    public void onDigitClick9(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 9);
+    }
+    public void onDigitClick0(ActionEvent actionEvent) {
+        digits.setText(digits.getText() + 0);
     }
 }
